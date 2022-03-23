af3dbc8e92818aa63d5ddbbf5eddc22a3b63e152
bozgen <bilalozgen00@gmail.com>
2022-03-23 21:19:09 +0300

    add comments

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,14 +1,15 @@
+ //This is the DatePicker component. It is imported and used in the ExploreContainer component.
+//This component receives setDate function as a prop which sets the date state in the ExploreContainer, on onIonChange event.
+
 import { IonDatetime } from "@ionic/react";
-import React from "react";
 
 export default function DatePicker(props: any){
-    function handleChange(value:any){
-        
+    function handleDateChange(value:any){
         props.setDate(value)
     }
     return(
         <div className="date-picker">
-            <IonDatetime className="datetime" onIonChange={(event)=> handleChange((event.detail.value))}></IonDatetime>
+            <IonDatetime className="datetime" onIonChange={(event)=> handleDateChange((event.detail.value))}></IonDatetime>
         </div>
     )
 }
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonButton, IonDatetime, IonPopover } from '@ionic/react';
+import { IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 //components
 import DatePicker from "./DatePicker";
@@ -8,12 +8,12 @@ interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   
-  const [dateShown, setDateShown] = React.useState(false)
-  const [date, setDate] = React.useState("");
+  const [dateShown, setDateShown] = React.useState(false) // Depending on this state, the datePicker and related elements are rendered or not.
+  const [date, setDate] = React.useState(""); // This state holds the value of the date on the datePicker
   
   
-  function formatDate(date : string){
-    let newDate = new Date(date);
+  function formatDate(date : string){ // state holds an unformatted date as a string.
+    let newDate = new Date(date);    // To cast it into a date object and format it,this function is used.
     return newDate.toLocaleDateString("en-US");
   }
   
@@ -21,17 +21,26 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
       <h2 className="title">PICK A DATE</h2>
-      {date != "" && <div className="date">
-        {formatDate(date)}
-      </div>}
-      {!dateShown && <IonButton onClick={() => setDateShown(true)} expand="block">
+      
+      {date != "" &&
+        <div className="date">
+          {formatDate(date)}
+        </div>}
+
+      {!dateShown &&
+        <IonButton onClick={() => setDateShown(true)} expand="block">
         <h5>Date Picker</h5>
         </IonButton>}
-      {dateShown && <h3 className="small-text">The date you picked is displayed above.</h3>}
+
+      {dateShown &&
+        <h3 className="small-text">
+          The date you picked is displayed above.
+        </h3>}
+
       <section className="datepicker">
-      {dateShown && <DatePicker setDate={setDate}/>}
+        {dateShown && <DatePicker setDate={setDate}/>}
       </section>
-      {dateShown && <IonButton onClick={()=>setDateShown(false)} ><h5>CLOSE</h5></IonButton>}
+        {dateShown && <IonButton onClick={()=>setDateShown(false)} ><h5>CLOSE</h5></IonButton>}
 
     </div>
   );
